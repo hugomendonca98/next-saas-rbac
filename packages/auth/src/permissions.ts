5eb3494851981028d8584b5edacd75537053c1b4
Hugo mendonca <hugomendonca9@gmail.com>
2024-05-04 16:42:44 -0300

    Definindo permissões do app

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -10,13 +10,21 @@ type PermissionByRole = (
 ) => void
 
 export const permissions: Record<role, PermissionByRole> = {
-  ADMIN: (_, builder) => {
+  ADMIN: (user, builder) => {
     builder.can('manage', 'all')
+
+    // Um admin não pode transferir uma organização para outro usuário, a não ser que o usuário seja o dono da organização.
+    builder.cannot(['transfer_ownership', 'update'], 'Organization')
+    builder.can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: { $eq: user.id },
+    })
   },
   MEMBER: (user, builder) => {
-    // builder.can('invite', 'User')
-    builder.can(['manage', 'get'], 'Project')
+    builder.can('get', 'User')
+    builder.can(['create', 'get'], 'Project')
     builder.can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } })
   },
-  BELLING: () => {},
+  BELLING: (_, builder) => {
+    builder.can('manage', 'Billing')
+  },
 }
