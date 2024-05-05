00adb8e9f5910f5d5c31de466ab8c603c728b262
Hugo mendonca <hugomendonca9@gmail.com>
2024-05-05 18:03:32 -0300

    Setup do app com Fastify

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAnilityFor, projectSchema } from '@saas/auth'
-
-const ability = defineAnilityFor({ role: 'MEMBER', id: 'user-id' })
-
-const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id-2' })
-
-console.log(ability.can('get', 'Billing'))
-console.log(ability.can('create', 'Invite'))
-console.log(ability.can('delete', project))
