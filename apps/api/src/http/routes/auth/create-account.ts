0539230df3df0af01f4b368bcf95cead10303ea5
Hugo mendonca <hugomendonca9@gmail.com>
2024-05-18 19:02:32 -0300

    Error Handling no Fastify

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -5,6 +5,8 @@ import z from 'zod'
 
 import { prisma } from '@/lib/prisma'
 
+import { BadRequestError } from '../_errors/bad-request-error'
+
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
@@ -21,9 +23,6 @@ export async function createAccount(app: FastifyInstance) {
           201: z.object({
             message: z.string(),
           }),
-          409: z.object({
-            message: z.string(),
-          }),
         },
       },
     },
@@ -35,9 +34,7 @@ export async function createAccount(app: FastifyInstance) {
       })
 
       if (userWithSameEmail) {
-        return reply
-          .status(409)
-          .send({ message: 'User with same e-mail already exists' })
+        throw new BadRequestError('User with same e-mail already exists')
       }
 
       const [, domain] = await email.split('@')
