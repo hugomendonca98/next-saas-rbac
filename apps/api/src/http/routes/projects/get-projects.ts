0ca5d1724bcde232ab1a3d5635fa8bd0944eaecb
Hugo <hugomendonca9@gmail.com>
2025-05-25 20:22:17 -0300

    Rota: Detalhes do projeto

---
+++ apps/api/src/http/routes/projects/get-projects.ts
@@ -0,0 +1,93 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { getUserPermissions } from '@/http/utils/get-user-permissions'
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:orgSlug/projects/:projectSlug',
+      {
+        schema: {
+          tags: ['projects'],
+          security: [{ bearerAuth: [] }],
+          summary: 'Get project details',
+          params: z.object({
+            orgSlug: z.string(),
+            projectSlug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              project: z.object({
+                description: z.string(),
+                id: z.string().uuid(),
+                name: z.string(),
+                slug: z.string(),
+                avatarUrl: z.string().nullable(),
+                organizationId: z.string().uuid(),
+                ownerId: z.string().uuid(),
+                ower: z.object({
+                  id: z.string().uuid(),
+                  name: z.string().nullable(),
+                  avatarUrl: z.string().nullable(),
+                }),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { orgSlug, projectSlug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(orgSlug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Project')) {
+          throw new UnauthorizedError(
+            `You're not a allowed to see this projects.`,
+          )
+        }
+
+        const project = await prisma.project.findUnique({
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            slug: true,
+            ownerId: true,
+            avatarUrl: true,
+            organizationId: true,
+            ower: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            slug: projectSlug,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!project) {
+          throw new BadRequestError(`Project nopt found`)
+        }
+
+        return reply.send({
+          project,
+        })
+      },
+    )
+}
